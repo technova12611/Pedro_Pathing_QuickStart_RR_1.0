b6b2947810dd646018eb49d959e5252036154c85
TechNova-1 <ftcteam12611@gmail.com>
2023-12-28 20:13:30 -0600

    upgraded MeepMeep to support RR 0.1.11

---
+++ MeepMeepV1Testing/src/main/java/com/roadrunner/simulation/MeepMeepV1Testing.java
@@ -0,0 +1,67 @@
+package com.roadrunner.simulation;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepV1Testing {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+
+        // Declare our first bot
+        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .build();
+
+        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(16.0, -62, Math.toRadians(90)))
+                                        .strafeToLinearHeading(new Vector2d(49.2,-36),Math.PI)
+                                        .strafeTo(new Vector2d(28.5,-24.5))
+                                        .strafeTo(new Vector2d(28.5,-12.0))
+                                  //      .strafeTo(new Vector2d(-50,-11.0))
+                                        .strafeToLinearHeading(new Vector2d(-55,-11.50), Math.PI)
+                                 //       .strafeTo(new Vector2d(-52,-10.50))
+                                        .strafeTo(new Vector2d(30,-12.0))
+                                        .strafeToLinearHeading(new Vector2d(48.5,-32.0), Math.PI)
+                                        .setReversed(false)
+                                        .strafeTo(new Vector2d(24,-12.0))
+                                 //       .strafeTo(new Vector2d(-50,-12.0))
+                                        .strafeToLinearHeading(new Vector2d(-56,-12.0), Math.PI)
+                                        .setReversed(true)
+                                        .strafeTo(new Vector2d(30,-12.0))
+                                        .strafeToLinearHeading(new Vector2d(48.5,-36.0), Math.PI)
+                                        .build());
+
+        // Declare out second bot
+        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+//        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(new Pose2d(30, 30, Math.toRadians(180)))
+//                .lineToX(0)
+//                .turn(Math.toRadians(90))
+//                .lineToY(0)
+//                .turn(Math.toRadians(90))
+//                .lineToX(30)
+//                .turn(Math.toRadians(90))
+//                .lineToY(30)
+//                .turn(Math.toRadians(90))
+//                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                // Add both of our declared bot entities
+                .addEntity(myFirstBot)
+//                .addEntity(mySecondBot)
+                .start();
+    }
+}
